4f8cad1ed53266c000e020b6513a66ad02526c88
Pavel Shnurov <pavel.shnurov@gmail.com>
2024-01-21 03:52:31 +0400

    feat: add 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const stream = fs.createReadStream('text.txt', 'utf-8');
+let data = '';
+
+stream.on('data', (chunk) => (data += chunk));
+stream.on('end', () => console.log(data));
+stream.on('error', (error) => console.log('Error', error.message));
